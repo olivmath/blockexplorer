790e496f6bbb8b8cad37b8c8d89620a1a0c2bb59
Lucas Oliveira <olivmath@protonmail.com>
2024-02-12 11:37:13 -0300

    block card ok - 2

--- src/block.js
+++ src/block.js
@@ -4,38 +4,38 @@ function BlockCard({ alchemy, number }) {
   const [block, setblock] = useState({});
 
   useEffect(() => {
-      async function getBlockByNumber(blockNumber) {
-        try {
-          const response = await alchemy.core.getBlock(blockNumber);
-          const block = {
-            number: blockNumber,
-            hash: response.hash,
-            difficulty: response.difficulty,
-            txCount: response.transactions.length,
-            date: new Date(response.timestamp * 1000).toLocaleString(), // Convertendo o timestamp para formato legível
-          }
-          setblock(block);
-          console.log(block)
-        } catch (error) {
-          alert('Erro ao buscar o número do bloco, detalhes no console');
-          console.error(error);
+    async function getBlockByNumber(blockNumber) {
+      try {
+        const response = await alchemy.core.getBlock(blockNumber);
+        const block = {
+          number: blockNumber,
+          hash: response.hash,
+          difficulty: response.difficulty,
+          txCount: response.transactions.length,
+          date: new Date(response.timestamp * 1000).toLocaleString(),
         }
+        setblock(block);
+        console.log(block)
+      } catch (error) {
+        alert('Erro ao buscar o número do bloco, detalhes no console');
+        console.error(error);
       }
+    }
 
-      if (number) { // Verifica se "number" está definido antes de chamar getBlockByNumber
-        getBlockByNumber(parseInt(number));
-      }
+    if (number) {
+      getBlockByNumber(parseInt(number));
+    }
 
-    }, [number, alchemy]); // Adiciona "number" às dependências do useEffect para reagir às mudanças
-  
+  }, [number, alchemy]);
+
   return (
-      <div className="block-card">
-        <h2>Block Number: {block.number}</h2>
-        <p>Hash Block: {block.hash}</p>
-        <p>Difficulty: {block.difficulty}</p>
-        <p>Transaction Count: {block.txCount}</p>
-        <p>date: {block.date}</p>
-      </div>
+    <div className="block-card">
+      <h2>Block Number: {block.number}</h2>
+      <p>Hash Block: {block.hash}</p>
+      <p>Difficulty: {block.difficulty}</p>
+      <p>Transaction Count: {block.txCount}</p>
+      <p>date: {block.date}</p>
+    </div>
   );
 }
 
