163363f30d6205871242b3efcb71099bebc1c6bd
Lucas Oliveira <olivmath@protonmail.com>
2024-02-12 12:10:45 -0300

    wallet card ok

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import WalletCard from './wallet';
 import TransactionCard from './transaction';
 import BlockCard from './block';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
+
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
 // You can read more about the packages here:
 //   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
@@ -88,7 +82,7 @@ function App() {
             type="text"
             value={input || ''}
             onChange={(e) => setInput(e.target.value)}
-            placeholder="Put: tx hash, block number or wallet address"
+            placeholder="Search by Tx hash / Block number / Wallet address"
             style={{ flexGrow: 1, marginRight: '10px', padding: '10px' }}
           />
           <button onClick={handlePaste} style={{ background: 'none', border: 'none', padding: '0', margin: '0 10px' }}>
--- src/wallet.jsx
+++ src/wallet.jsx
@@ -2,33 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { ethers } from "ethers";
 
 function WalletCard({ alchemy, address }) {
-    const [walletData, setWalletData] = useState({});
+    const [wallet, setWallet] = useState(null);
 
     useEffect(() => {
         async function getWalletBalance(address) {
-            let balance
+            let response
             try {
-                balance = await alchemy.core.getBalance(address, "latest");
+                response = await alchemy.core.getBalance(address, "latest");
             } catch (error) {
                 alert('Erro ao buscar o saldo da carteira, detalhes no console')
                 console.error(error);
             }
-            const formattedBalance = ethers.getBigInt(balance.value._hex);
-            setWalletData({
-                address: `${address.substring(0, 6)}...${address.substring(address.length - 4)}`,
+            const formattedBalance = ethers.getBigInt(response._hex);
+            const walletData = {
+                address: address,
                 balance: formattedBalance
-            });
+            }
+            setWallet(walletData);
+            console.log(walletData);
         }
 
         getWalletBalance(address)
-    }, [alchemy, address]);
+    }, [address, alchemy]);
 
-    return (
-        <div className="wallet-card">
-            <h2>Wallet Address: {walletData.address}</h2>
-            <p>Balance: {walletData.balance} ETH</p>
-        </div>
-    );
+    if (!wallet) {
+        return (
+            <div className="wallet-card">
+                <h2>Carregando dados da Wallet...</h2>
+            </div>
+        )
+    } else {
+        return (
+            <div className="wallet-card">
+                <h2>Wallet Address: {`${wallet.address.substring(0, 6)}...${wallet.address.substring(wallet.address.length - 4)}`}</h2>
+                <p>Balance: {parseInt(wallet.balance) / 10e18} ETH</p>
+            </div>
+        );
+    }
 }
 
 export default WalletCard;
